46badf7e3bbcb6682c91fa22a772bd33cff48c32
fussy113 <s1220080@gmail.com>
2018-04-11 00:40:16 +0900

    let's cook

--- app.rb
+++ app.rb
@@ -1,4 +1,8 @@
 # -*- encoding: utf-8 -*-
+require 'pry'
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/poltergeist'
 require 'sinatra'
 require 'sinatra/activerecord'
 require_relative 'models/iidx_level8'
@@ -6,6 +10,14 @@ require_relative 'models/iidx_level9'
 require_relative 'models/iidx_level10'
 require_relative 'models/iidx_level11'
 require_relative 'models/iidx_level12'
+include Capybara::DSL
+
+def initialize() Capybara.register_driver :poltergeist_debug do |app|
+	Capybara::Poltergeist::Driver.new(app, :inspector => true)
+end
+Capybara.default_driver = :poltergeist
+Capybara.javascript_driver = :poltergeist
+end
 
 get "/" do
 	"Hello!!"
@@ -69,4 +81,21 @@ end
 
 get "/iidx_level12s/:id" do
 	Iidx_Level12.find(params[:id]).to_json
+end
+
+get "/cook/:search" do
+  cook_ary = Array.new(5,2)
+
+  url = "https://cookpad.com/search/#{params[:search]}"
+  #日本語url
+	ja_url=URI.escape(url)
+	page.driver.headers = { "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36" }
+	visit(ja_url)
+
+	doc = Nokogiri::HTML.parse(page.html)
+
+  doc.css('.recipe-title').children.each_with_index { |recipe,i |
+		cook_ary[i,0] = recipe.text
+    cook_ary[i,1] = recipe[:href]
+  }
 end
